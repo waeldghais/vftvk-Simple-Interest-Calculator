03a224c2d546ef42314e53762e24ee3ee59c81f0
Wael Dghais <waeldghaisdg@gmail.com>
2021-11-16 14:42:38 +0100

    correction

--- script.js
+++ script.js
@@ -3,18 +3,26 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
+    if (principal <= 0) {
+      alert("Enter a positive number")
+      document.getElementById("principal").focus()
+      return
+    }
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
-    if(principal<=0){
-        alert("Enter a positive number");
-    }else{
-        document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
-    }
     
-    
+    document.getElementById("result").innerHTML=
+        "If you deposit <span class='indicateValues'>"+principal+ "</span>,\<br\>"+
+        "at an interest rate of <span class='indicateValues'>"+rate+" %</span>,\<br\>"+
+        "You will receive an amount of <span class='indicateValues'>"+interest+"</span>,\<br\>"+
+        "in the year <span class='indicateValues'>"+year+"</span>\<br\>"
 }
+
+    
+    
+
 
 
 function updateRate() 
